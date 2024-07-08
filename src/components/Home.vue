995f973362ae0aa9d591e553a175d1f587312813
Mariia Schmidt <snowz0230@gmail.com>
2024-07-08 23:01:51 +0400

    Подключил бэк

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+
+<template>
+    <h1>Hello</h1>
+</template>
+
+<script>
+fetch('https://localhost:7103/api/Singer/getSingerList')
+    .then(res => console.log(res.json()))
+
+console.log(1)
+</script>
